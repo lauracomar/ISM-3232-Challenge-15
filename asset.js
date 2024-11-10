236eee1274660ea01432831db25aeb06711c2a8d
Laura Comar <lautmcomar@gmail.com>
2024-11-10 15:55:24 -0500

    Create asset module with asset list and getAssetById function

---
+++ asset.js
@@ -0,0 +1,12 @@
+//Create the Asset Module
+// create assets array, and define assets:
+export const assets = [
+    { id: 1, name: "Coca Cola", type: "stock", price: 70, quantity: 50 },
+    { id: 2, name: "Tesla", type: "stock", price: 300, quantity: 50 },
+    { id: 3, name: "Berkshire Hathaway", type: "stock", price: 400, quantity: 50 },
+];
+
+// export function to get asset by id
+export function getAssetById(id) {
+    return assets.find(asset => asset.id === id);
+};
